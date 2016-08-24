8dbf29a7aa3daaaf03db886b4a32beeec3cabe8d
Robert Kenny <robert.kenny@guardian.co.uk>
2016-08-24 10:18:01 +0100

    adds examples

---
+++ examples/example.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/guardian/gocapiclient"
+	"github.com/guardian/gocapiclient/queries"
+	"log"
+)
+
+func main() {
+	client := gocapiclient.NewGuardianContentClient("https://content.guardianapis.com/", "my-api-key")
+	searchQuery(client)
+	searchQueryPaged(client)
+	itemQuery(client)
+}
+
+func searchQueryPaged(client *gocapiclient.GuardianContentClient) {
+	searchQuery := queries.NewSearchQuery()
+
+	showParam := queries.StringParam{"q", "sausages"}
+	params := []queries.Param{&showParam}
+
+	searchQuery.Params = params
+
+	iterator := client.SearchQueryIterator(searchQuery)
+
+	for results := range iterator {
+		fmt.Println("----------------- New Page -----------------------")
+		for _, v := range results {
+			fmt.Println(v.ID)
+		}
+	}
+}
+
+func searchQuery(client *gocapiclient.GuardianContentClient) {
+	searchQuery := queries.NewSearchQuery()
+
+	showParam := queries.StringParam{"q", "sausages"}
+	params := []queries.Param{&showParam}
+
+	searchQuery.Params = params
+
+	err := client.GetResponse(searchQuery)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(searchQuery.Response.Status)
+	fmt.Println(searchQuery.Response.Total)
+
+	for _, v := range searchQuery.Response.Results {
+		fmt.Println(v.ID)
+	}
+}
+
+func itemQuery(client *gocapiclient.GuardianContentClient) {
+	itemQuery := queries.NewItemQuery("technology/2016/aug/12/no-mans-sky-review-hello-games")
+
+	showParam := queries.StringParam{"show-fields", "all"}
+	params := []queries.Param{&showParam}
+
+	itemQuery.Params = params
+
+	err := client.GetResponse(itemQuery)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(itemQuery.Response.Status)
+	fmt.Println(itemQuery.Response.Content.WebTitle)
+}
